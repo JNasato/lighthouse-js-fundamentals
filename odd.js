bc54e14470d0df17d8c5e42320157fe3b6d64bc9
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-02-26 07:01:12 +0000

    Odd Checker assignment

---
+++ odd.js
@@ -0,0 +1,15 @@
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(3));
+
+console.log(isOdd(6));
+
+console.log(isOdd(19));
+
+console.log(isOdd(42));
+
+console.log(isOdd(99));
+
+console.log(isOdd(500));
