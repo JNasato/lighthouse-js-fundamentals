497c69b38bef42b0daa4c89cecf07e93913f390f
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-02-26 06:51:31 +0000

    isEven functions

--- hello.js
+++ hello.js
@@ -4,3 +4,14 @@ const sayHello  = function (name) {
 
 sayHello("Jebs");
 
+//Console Log
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+//Return
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
