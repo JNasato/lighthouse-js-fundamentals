33e818caa4528fd0ba302aa6be145e8764611fda
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-02-28 18:33:29 +0000

    packing-list while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,16 @@
+//Array - For Loop
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+//Array - While Loop
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
